ed78a64ccd2d380dbffbd7e860558c141d7a294a
Santiago8888 <santiago.marti67@gmail.com>
2021-10-27 11:47:16 -0500

    Frequence & prescence penalties.

--- talk-talk/main.py
+++ talk-talk/main.py
@@ -40,10 +40,10 @@ def learn(word, id):
         engine='davinci',
         prompt=prompt,
         temperature=0.5,
-        max_tokens=73,
+        max_tokens=100,
         top_p=1,
-        frequency_penalty=0, # TODO: Test other values.
-        presence_penalty=0,  # TODO: Test other values.
+        frequency_penalty=0.25,
+        presence_penalty=0.25,
         stop=["\n"]  
     )
 
